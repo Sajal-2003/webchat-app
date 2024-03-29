e2560d31cbd8616c504526d8d03022ea9e2e5afb
Sajal Aggarwal <sajal72003@gmail.com>
2024-03-29 16:01:21 +0530

    deployment commit

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://webchat-app-k8yk.onrender.com/login", {
+      const socket = io("https://webchat-app-k8yk.onrender.com", {
         query: {
           userId: authUser._id,
         },
